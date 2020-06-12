58b87897db3178a41c9b3e3d724fd05222d774c6
ywon0008 <ywon0008@student.monash.edu>
2020-06-12 22:54:24 +1000

    sample decks are now showing

--- client/src/component/products.js
+++ client/src/component/products.js
@@ -2,10 +2,25 @@ import React from 'react';
 import { useQuery } from '@apollo/react-hooks';
 import { GET_PRODUCTS } from "../query/graph";
 import CardDeck from 'react-bootstrap/CardDeck'
-import CardColumns from 'react-bootstrap/CardColumns'
 
 import Card from 'react-bootstrap/Card'
 
+
+const wrapCards = (cards) => {
+  const deck = [];
+  cards.forEach((card, i) => {
+    const index = Math.floor(i/5);
+    if (deck.length === index) {
+      deck.push([]);
+    }
+    deck[index].push(card);
+  })
+  return deck.map(cards => 
+    <CardDeck>
+      {cards}
+    </CardDeck>
+  );
+}
 
 const Products = (props) => {
   const { loading, error, data } = useQuery(GET_PRODUCTS);
@@ -13,220 +28,23 @@ const Products = (props) => {
   if (loading) return 'Loading...';
   if (error) return `Error! ${error.message}`;
 
+  const cards = data.getProducts.map(product => 
+    <Card>
+      <Card.Img src={product.img} />
+      <Card.Body>
+        <Card.Text>
+          {product.name}
+        </Card.Text>
+      </Card.Body>
+      <Card.Footer>
+        <small className="text-muted">Last updated 3 mins ago</small>
+      </Card.Footer>
+    </Card>
+  );
   return (
-    // <CardDeck>
-    //       <Card style={{ width: '4rem' }}>
-    //         <Card.Img variant="top" src="https://interactive-examples.mdn.mozilla.net/media/examples/grapefruit-slice-332-332.jpg" />
-    //         <Card.Body>
-    //           <Card.Title>{data.getProducts[0].brand}</Card.Title>
-    //           <Card.Title>{data.getProducts[0].brand}</Card.Title>
-    //           <Card.Text>
-    //             Jello how are yu
-    //           </Card.Text>
-    //         </Card.Body>
-    //         <Card.Footer>
-    //           <small className="text-muted">Last updated 3 mins ago</small>
-    //         </Card.Footer>
-    //       </Card>
-    //       <Card style={{ width: '4rem' }}>
-    //         <Card.Img variant="top" src="https://interactive-examples.mdn.mozilla.net/media/examples/grapefruit-slice-332-332.jpg" />
-    //         <Card.Body>
-    //           <Card.Title>{data.getProducts[0].brand}</Card.Title>
-    //           <Card.Title>{data.getProducts[0].brand}</Card.Title>
-    //           <Card.Text>
-    //             Jello how are yu
-    //           </Card.Text>
-    //         </Card.Body>
-    //         <Card.Footer>
-    //           <small className="text-muted">Last updated 3 mins ago</small>
-    //         </Card.Footer>
-    //       </Card>
-    // </CardDeck>
-    <div>
-      <CardColumns>
-  <Card>
-    <Card.Img variant="top" src="holder.js/100px160" />
-    <Card.Body>
-      <Card.Title>Card title that wraps to a new line</Card.Title>
-      <Card.Text>
-        This is a longer card with supporting text below as a natural lead-in to
-        additional content. This content is a little bit longer.
-      </Card.Text>
-    </Card.Body>
-  </Card>
-  <Card className="p-3">
-    <blockquote className="blockquote mb-0 card-body">
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere
-        erat a ante.
-      </p>
-      <footer className="blockquote-footer">
-        <small className="text-muted">
-          Someone famous in <cite title="Source Title">Source Title</cite>
-        </small>
-      </footer>
-    </blockquote>
-  </Card>
-  <Card>
-    <Card.Img variant="top" src="holder.js/100px160" />
-    <Card.Body>
-      <Card.Title>Card title</Card.Title>
-      <Card.Text>
-        This card has supporting text below as a natural lead-in to additional
-        content.{' '}
-      </Card.Text>
-    </Card.Body>
-    <Card.Footer>
-      <small className="text-muted">Last updated 3 mins ago</small>
-    </Card.Footer>
-  </Card>
-  <Card bg="primary" text="white" className="text-center p-3">
-    <blockquote className="blockquote mb-0 card-body">
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere
-        erat a ante.
-      </p>
-      <footer className="blockquote-footer">
-        <small className="text-muted">
-          Someone famous in <cite title="Source Title">Source Title</cite>
-        </small>
-      </footer>
-    </blockquote>
-  </Card>
-  <Card className="text-center">
-    <Card.Body>
-      <Card.Title>Card title</Card.Title>
-      <Card.Text>
-        This card has supporting text below as a natural lead-in to additional
-        content.{' '}
-      </Card.Text>
-      <Card.Text>
-        <small className="text-muted">Last updated 3 mins ago</small>
-      </Card.Text>
-    </Card.Body>
-  </Card>
-  <Card>
-    <Card.Img src="holder.js/100px160" />
-  </Card>
-  <Card className="text-right">
-    <blockquote className="blockquote mb-0 card-body">
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere
-        erat a ante.
-      </p>
-      <footer className="blockquote-footer">
-        <small className="text-muted">
-          Someone famous in <cite title="Source Title">Source Title</cite>
-        </small>
-      </footer>
-    </blockquote>
-  </Card>
-  <Card>
-    <Card.Body>
-      <Card.Title>Card title</Card.Title>
-      <Card.Text>
-        This is a wider card with supporting text below as a natural lead-in to
-        additional content. This card has even longer content than the first to
-        show that equal height action.
-      </Card.Text>
-      <Card.Text>
-        <small className="text-muted">Last updated 3 mins ago</small>
-      </Card.Text>
-    </Card.Body>
-  </Card>
-  <Card>
-    <Card.Body>
-      <Card.Title>Card title</Card.Title>
-      <Card.Text>
-        This is a wider card with supporting text below as a natural lead-in to
-        additional content. This card has even longer content than the first to
-        show that equal height action.
-      </Card.Text>
-      <Card.Text>
-        <small className="text-muted">Last updated 3 mins ago</small>
-      </Card.Text>
-    </Card.Body>
-  </Card>
-  <Card>
-    <Card.Body>
-      <Card.Title>Card title</Card.Title>
-      <Card.Text>
-        This is a wider card with supporting text below as a natural lead-in to
-        additional content. This card has even longer content than the first to
-        show that equal height action.
-      </Card.Text>
-      <Card.Text>
-        <small className="text-muted">Last updated 3 mins ago</small>
-      </Card.Text>
-    </Card.Body>
-  </Card>
-  <Card>
-    <Card.Body>
-      <Card.Title>Card title</Card.Title>
-      <Card.Text>
-        This is a wider card with supporting text below as a natural lead-in to
-        additional content. This card has even longer content than the first to
-        show that equal height action.
-      </Card.Text>
-      <Card.Text>
-        <small className="text-muted">Last updated 3 mins ago</small>
-      </Card.Text>
-    </Card.Body>
-  </Card>
-  <Card>
-    <Card.Body>
-      <Card.Title>Card title</Card.Title>
-      <Card.Text>
-        This is a wider card with supporting text below as a natural lead-in to
-        additional content. This card has even longer content than the first to
-        show that equal height action.
-      </Card.Text>
-      <Card.Text>
-        <small className="text-muted">Last updated 3 mins ago</small>
-      </Card.Text>
-    </Card.Body>
-  </Card>
-  <Card>
-    <Card.Body>
-      <Card.Title>Card title</Card.Title>
-      <Card.Text>
-        This is a wider card with supporting text below as a natural lead-in to
-        additional content. This card has even longer content than the first to
-        show that equal height action.
-      </Card.Text>
-      <Card.Text>
-        <small className="text-muted">Last updated 3 mins ago</small>
-      </Card.Text>
-    </Card.Body>
-  </Card>
-  <Card>
-    <Card.Body>
-      <Card.Title>Card title</Card.Title>
-      <Card.Text>
-        This is a wider card with supporting text below as a natural lead-in to
-        additional content. This card has even longer content than the first to
-        show that equal height action.
-      </Card.Text>
-      <Card.Text>
-        <small className="text-muted">Last updated 3 mins ago</small>
-      </Card.Text>
-    </Card.Body>
-  </Card>
-  <Card>
-    <Card.Body>
-      <Card.Title>Card title</Card.Title>
-      <Card.Text>
-        This is a wider card with supporting text below as a natural lead-in to
-        additional content. This card has even longer content than the first to
-        show that equal height action.
-      </Card.Text>
-      <Card.Text>
-        <small className="text-muted">Last updated 3 mins ago</small>
-      </Card.Text>
-    </Card.Body>
-  </Card>
-</CardColumns>
-</div>
+    <>
+    {wrapCards(cards)}
+    </>
   );
 }
 
