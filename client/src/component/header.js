b6c858fcffc8279edf926f10143f2ac763a7b8bf
ywon0008 <ywon0008@student.monash.edu>
2020-06-15 16:27:25 +1000

    disable unusable func

--- client/src/component/header.js
+++ client/src/component/header.js
@@ -17,11 +17,11 @@ const Header = (props) => {
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
             <Nav.Link as={Link} to="/home">Home</Nav.Link>
-            <Nav.Link href="#link">Products</Nav.Link>
+            <Nav.Link as={Link} to="/products">Products</Nav.Link>
           </Nav>
           <Form inline>
-            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-            <Button variant="outline-success">Search</Button>
+            <FormControl disabled type="text" placeholder="Search" className="mr-sm-2" />
+            <Button disabled variant="outline-success">Search</Button>
           </Form>
         </Navbar.Collapse>
       </Navbar>
